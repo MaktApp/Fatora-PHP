9ab2c97ddbd7871cea238a3eb18868ecb4018399
Fatora Invoicing and Payment gateway <59866678+MaktApp@users.noreply.github.com>
2020-05-14 16:10:18 +0300

    Update success.php

--- MiniStorePHP/success.php
+++ MiniStorePHP/success.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>MiniStoreJS</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="icon" href="images/fatora.ico" type="image/x-icon" />
-    <link rel="shortcut icon" href="images/fatora.ico" type="image/x-icon" />
-    
-    <meta charset="UTF-8"><link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
-	<script type="text/javascript" src="js/payment.js"></script>
-	
-<script type="text/javascript">
-$(document).ready(function(){
+<?php
 
 /*
     This is requested by Fator gateway after payment has been done,
@@ -22,168 +6,114 @@ $(document).ready(function(){
     This function sends a request to ensure that Fatora gateway which issued it,
     If the result of the response was successfully, the action processes the following:
     1- converting The customer's order into a paid status
-    2- show success page with successfull msg
+    2- show success page successful msg
     If the result of the response was failed, the action
     1- returns the error page with failure message
 */
+if ( isset($_REQUEST['transactionid']) || isset($_REQUEST['paymentID']) ) 
+{
+    
 
-var orderId = getUrlVars()["orderId"];
-var checkStatus = getUrlVars()["checkStatus"];
+    $result = CheckStatus();
 
-if (checkStatus == undefined) //this parameter has value after checkstatus is done successfully
-                             
-	{
-	var dataToPost = {
-				"token": "5BC4B469-1EC0-4222-AF1F-7CE632F29A23",
-				"orderId": orderId,			 
-			  };
+    if ($result== 1)
+       echo "payment done successfully!";
+    elseif ($result == 0)
+     echo "no payment found for this order";
+    else 
+    check_response($result);
 
-		    $.ajax({            
-		    url: 'https://maktapp.credit/v3/CheckStatus',
-		    data: dataToPost,
-		    type: "POST",
-		    dataType: "json",
-		   
-		    success: function (res) {      
-		    
-		    /*
-                
-                The above request will returns JSON structured if Fatora gateway find the payment:
 
-                { 
-                    "result": 1,
-                    "payment":
-                    {
-                        "transactionID": "XXXXXXX",
-                        "amount": XXXX,
-                        "currencyCode":  "XXX",
-                        "customerEmail": "XXXX",
-                        "customerPhone": "XXXX",
-                        "customerName":  "XXXX",
-                        "paymentDate":   "XXXX",
-                        "paymentstate":  "XXXX" [SUCCESS, PENDING,FAILURE], 
-                        "auth" : "XXXX",
-                        "mode": "XXX" [Live, Test] ,
-                        "ExchangeRate":0,
-                        "token":null,
-                        "description":"Transation Successfull",
-                        "refundState":false,
-                        "refundstatus":null,
-                        "refundDescription":null,
-                        "refundTransactionId":null
-                    } 
-                }
-                Response if Fatora gatway dos not find the payment:
-                { 
-                    "result": 0
-                }
-                Response in case one of the request parameters is null or not valid:
 
-                { 
-                    "result": X
-                } [-1, -2, -3, -6, -8, -10, -20, -21 ]
-                                
-                */		
-		    var response = CheckResponseStatus(res.result);
-		    if (res.result == 1)
-             /*
-             1. confirm  customer`s order status as paid
-             order = getOrderById(orderId);
-             order.status = "done";
-             ............ your code is here to save order.......
-             
-             2. return success page
-            */
-             	 window.location.href = "success.php?orderId="+ orderId + "&checkStatus=1";   
-            else
-                  window.location.href = "error.php?error="+response;                        
-		        },
-		    error: function (result) {   
-		            alert("error: " + result);
-		        }
-		    });        
-	
-	}
-
-});
-
-function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
 }
 
-function  CheckResponseStatus(result)
+
+
+function CheckStatus()
 {
-    var msg = "";
-    switch (result)
-    {   
+        $url = 'https://maktapp.credit/v3/CheckStatus';
+        $data = array(
+        'token'           => 'E4B73FEE-F492-4607-A38D-852B0EBC91C9', 
+        'orderId'         => 123
+);
+
+        $response = curl_post( $url , $data );
+        $data_json_decode = json_decode($response);
+        $result = $data_json_decode->{'result'};
+
+        return  $result;
+}
+
+function curl_post($url, array $post = NULL, array $options = array())
+{ 
+        $defaults = array(
+        CURLOPT_POST           => 1,
+        CURLOPT_HEADER         => 0,
+        CURLOPT_URL            => $url,
+        CURLOPT_FRESH_CONNECT  => 1,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_FORBID_REUSE   => 1,
+        CURLOPT_TIMEOUT        => 500,
+        CURLOPT_POSTFIELDS     => http_build_query($post)
+        );
+        $ch = curl_init();
+        curl_setopt_array($ch, ($options + $defaults));
+        if( ! $result = curl_exec($ch))
+        {
+        trigger_error(curl_error($ch));
+        }
+        curl_close($ch);
+        return $result;
+}
+
+function check_response($result){
+    
+    switch ($result){
+        
         case -1:
-            msg = "Unauthorized -- API key is invaild(not Valid GUID), or no merchant for this token";
+            $url = "/fatora/error.php";
+            $msg = "Unauthorized API key is wrong, No merchant for this token";
             break;
         case -2:
-            msg = "Not Found -- The specified orderId could not be found.";
+            $url = "/fatora/error.php";
+            $msg = "Not Found -- The specified orderId could not be found.";
             break;
         case -3:
-            msg = "Not Support -- merchnat application doesn't support the sent currency code.";
+          $url = "/fatora/error.php";
+          $msg = "Not Support -- The currency requested is not support by merchant`s application";
             break;
-        case -7:
-            msg = "Not Found -- there isn't any recurring or autosave payment in Fatora gateway for this merchant with the request parameter.";
+        case -6:
+          $url = "/fatora/error.php";
+          $msg = "Not Found -- The specified process could not be found, and could not get new payment.";
             break;
         case -8:
-            msg = "Invalid -- Token is not valid guid.";
+            $url = "/fatora/error.php";
+            $msg = "Invalid -- Token is not valid guid.";
             break;
         case -10:
-            msg = "Bad Request -- required parameters requested aren't sent in request.";
+          $url = "/fatora/error.php";
+          $msg = "Bad Request -- Your request is invalid, parameters requested are not found.";
             break;
         case -20:
-            msg = "Not Found -- There isn't application data for the merchant for the sent currency in payment gateway.";
+          $url = "/fatora/error.php";
+          $msg = "Not Found -- There is not Merchant`s data aginst currency requested.";
             break;
         case -21:
-            msg = "Not Support -- payment gateway doesn't support void payment";
+          $url = "/fatora/error.php";
+          $msg = "Not Support -- payment getway not support void payment";
             break;
         default:
-            msg = "success";
+            $url = $result;
+            $msg = "success";
             break;
-        }
-       
-        
-        return msg;
-} 
+    }
+  
+    echo "
+    <script type=\"text/javascript\">
+   
+    window.location.href ='".$url."?msg=".$msg."'
 
-</script>
-<style>
-    
-.center {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 100%;
-}
-.fa-6 {
-    font-size: 7cm;
-    color: rgb(90,201,157);
-}
-
-</style>
-</head>
-    
-    <body>
-    
-        <div class = "row" >
-        <div class = "col-md-4">
-           
-	   </div>
-	   <div class = "col-md-4">
-	       <i class="fa fa-check-circle fa-6 center" aria-hidden="true"></i>
-           <h3 class = "center">Payment success!</h3>
-	   </div>
-	   <div class = "col-md-4">
-	       
-	   </div>
-	   </div>
-	   
-    </body>
- </html>
+    </script>
+  ";
+  }
+?>
